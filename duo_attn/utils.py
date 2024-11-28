b4b899ac34b679f88188c1dc358844034ace2fcb
Matthew Habtezgi <mhbtz@mit.edu>
2024-11-28 00:47:13 -0500

    ship

--- duo_attn/utils.py
+++ duo_attn/utils.py
@@ -21,7 +21,7 @@ def parse_args():
     parser.add_argument(
         "--dataset_name",
         type=str,
-        default="/content/llava/passkey_images.gz",
+        default="/content/llava/passkey_images.json.gz",
     )
     parser.add_argument("--dataset_format", type=str, default="multiple_passkey")
     parser.add_argument("--split", type=str, default="train")
